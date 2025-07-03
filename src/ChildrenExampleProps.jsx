3d30e2e188672e941fd7badcedb98928f7649817
Jarif Foysal <jarifsudin6@gmail.com>
2025-07-04 00:56:55 +0600

    h

---
+++ src/ChildrenExampleProps.jsx
@@ -0,0 +1,31 @@
+// // MyComponent.js
+// import React from "react";
+
+// function MyComponent({ children }) {
+//   return (
+//     <div style={{ border: "2px solid blue", padding: "10px" }}>
+//       <h3>Inside MyComponent:</h3>
+//       {children}
+//     </div>
+//   );
+// }
+
+// export default MyComponent;
+
+// // App.js (or Parent Component)
+// import React from "react";
+// import MyComponent from "./MyComponent";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Parent Component</h1>
+//       <MyComponent>
+//         <p>This content is passed as <strong>children</strong>.</p>
+//         <button>Click Me</button>
+//       </MyComponent>
+//     </div>
+//   );
+// }
+
+// export default App;
